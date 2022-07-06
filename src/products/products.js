ebe1044b28dd7e4e51470b2ca904426c906a4f18
Voorgo <serbancatalin10@yahoo.com>
2022-07-06 22:30:35 +0300

    Add products objects

---
+++ src/products/products.js
@@ -0,0 +1,129 @@
+export const products = [
+  {
+    name: "Gildan Red T-shirt",
+    price: 15.9,
+    gender: "men",
+    image: "images/men/0.png",
+  },
+  {
+    name: "Gildan White T-shirt",
+    price: 10.9,
+    gender: "men",
+    image: "images/men/1.png",
+  },
+  {
+    name: "Fairdes  T-shirt",
+    price: 12,
+    gender: "men",
+    image: "images/men/2.png",
+  },
+  {
+    name: "White Polo-shirt",
+    price: 20,
+    gender: "men",
+    image: "images/men/3.png",
+  },
+  {
+    name: "Gildan Yellow T-shirt",
+    price: 9.9,
+    gender: "men",
+    image: "images/men/4.png",
+  },
+  {
+    name: "Fairdes Gray T-shirt",
+    price: 11.9,
+    gender: "men",
+    image: "images/men/5.png",
+  },
+  {
+    name: "White Long-sleeves shirt",
+    price: 20.85,
+    gender: "men",
+    image: "images/men/6.png",
+  },
+  {
+    name: "Cooper White T-shirt",
+    price: 18.5,
+    gender: "men",
+    image: "images/men/7.png",
+  },
+  {
+    name: "Sportswear yellow T-shirt",
+    price: 16,
+    gender: "men",
+    image: "images/men/8.png",
+  },
+  {
+    name: "Red Polo-shirt",
+    price: 13.2,
+    gender: "men",
+    image: "images/men/9.png",
+  },
+  {
+    name: "Gray long-sleeves shirt",
+    price: 19.9,
+    gender: "women",
+    image: "images/women/0.png",
+  },
+  {
+    name: "Blue Space T-shirt",
+    price: 9,
+    gender: "women",
+    image: "images/women/1.png",
+  },
+  {
+    name: "Red V-neck T-shirt",
+    price: 12.5,
+    gender: "women",
+    image: "images/women/2.png",
+  },
+  {
+    name: "Simple White T-shirt",
+    price: 10.5,
+    gender: "women",
+    image: "images/women/3.png",
+  },
+  {
+    name: "Olive Blouse",
+    price: 16,
+    gender: "women",
+    image: "images/women/4.png",
+  },
+  {
+    name: "Colorful Dress",
+    price: 30,
+    gender: "women",
+    image: "images/women/5.png",
+  },
+  {
+    name: "Pink Dress",
+    price: 27.6,
+    gender: "women",
+    image: "images/women/6.png",
+  },
+  {
+    name: "Yellow Long Dress",
+    price: 35.9,
+    gender: "women",
+    image: "images/women/7.png",
+  },
+  {
+    name: "Red Top",
+    price: 11,
+    gender: "women",
+    image: "images/women/8.png",
+  },
+  {
+    name: "Red Medium Dress",
+    price: 33,
+    gender: "women",
+    image: "images/women/9.png",
+  },
+];
+
+export const bestSellers = [
+  products[15],
+  products[0],
+  products[2],
+  products[18],
+];
